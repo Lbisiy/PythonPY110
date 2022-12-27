c85328beddd74adbf8a36585880f2b2605f4dca3
Marat <maratsafin.let@gmail.com>
2022-12-27 18:23:51 +0100

    Лаб4

--- Занятие4/Лабораторные_задания/task1_1/main.py
+++ Занятие4/Лабораторные_задания/task1_1/main.py
@@ -2,7 +2,16 @@ import json
 import re
 
 BOOKS_FILE = "books.md"
-BOOK_REGEX = ...  # TODO записать ругулярное выражения для поиска книги
+BOOK_REGEX = r"####\s+(?P<position>\d+)\.\s+\[(?P<book>([-\w:'\s])+)\]\((?P<book_url>https:\/\/a\w+\.\w+\/\w+)\)\s+by\s+(?P<author>([\s]?[&]?[\s+]?[A-Z]['.]?\w*[.]?){2,})\s+\((?P<recommended>\d+\.\d+%)\s+\w+\)\n+!\[\]\((?P<cover_url>https:\/\/\w\w\w\.\w+\.\w+\/\w+\/\w+\/\d+\.\w+[#]\w+)\)\s+\"(?P<description>(\w+[-—:()&'`’“”,!;?.\s]*)*)"
+
+
+# reg_group(position) = (?P<position>\d+)
+# reg_group(book) = (?P<book>([-\w:'\s])
+# reg_group(book_url) = (?P<book_url>https:\/\/a\w+\.\w+\/\w+)
+# reg_group(author) = (?P<author>([\s]?[&]?[\s+]?[A-Z]['.]?\w*[.]?){2,})
+# reg_group(recommended) = (?P<recommended>\d+\.\d+%)
+# reg_group(cover_url) = (?P<cover_url>https:\/\/\w\w\w\.\w+\.\w+\/\w+\/\w+\/\d+\.\w+[#]\w+)
+# reg_group(description) = (?P<description>(\w+[-—:()&'`’“”,!;?.\s]*)*)
 
 
 def task():
